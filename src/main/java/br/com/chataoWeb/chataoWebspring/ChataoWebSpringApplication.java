cdf304a78513026283398206b21ba8eedbec8364
Kauai Guarilha Freitas <kauai.guarilha@hotmail.com>
2019-10-14 22:48:19 -0300

    primeiro commit

---
+++ src/main/java/br/com/chataoWeb/chataoWebspring/ChataoWebSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.chataoWeb.chataoWebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChataoWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChataoWebSpringApplication.class, args);
+	}
+
+}
